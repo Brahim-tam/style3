dc18d6147c443c8b2ccb25f76a776fe1160518ca
Brahim-tam <80415321+Brahim-tam@users.noreply.github.com>
2021-06-03 07:36:48 +0100

    رفع الملفات

---
+++ js/script.js
@@ -0,0 +1,46 @@
+   //لفتح القائمة الجانبية 
+   function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  //لغلق القائمة الجانبية 
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+  $(function() {
+    var header = $("#headerr");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 200) {
+            header.removeAttr('id','headerr').attr('id', 'header');
+            $("#branding").removeAttr("style");
+            
+            $( ".dFixed" ).removeClass( ".dFixed" ).addClass( "Fixedn" );
+            $( ".nFixed" ).removeClass( ".nFixed" ).addClass( "Fixedd" );
+        } else {
+            header.removeAttr('id','header').attr('id', 'headerr');
+            $("#branding").attr("style","flex-grow:8;");
+            
+            $(".Fixedn").removeClass("Fixedn");
+            $(".Fixedn").removeClass("Fixedd");
+        }
+    });
+});
+
+$(function checkPosition() {
+  if (window.matchMedia('(max-height: 450px)').matches) {
+      $('#mySidenav').prepend(' <a class="lLink hide" href="index.html"><li class="current lItem ">الصفحة الرئيسية</li></a>')
+      $('#mySidenav').prepend(' <a class="lLink hide" href="index.html"><li class="current lItem ">معرض الأعمال</li></a>')
+      
+  } else {
+    $('#mySidenav').remove('.lItem')
+  }
+
+  $('#toUp').click(()=>{
+    $("html,body").animate({scrollTop:0},2700);
+    return false ;
+  })
+});
+
